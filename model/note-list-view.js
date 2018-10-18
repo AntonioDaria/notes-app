ce4ad819821ca13800b7c11dc6bffab191bd1d94
Antonio <antoniodaria@msn.com>
2018-10-18 15:39:06 +0100

    Step 8 completed

--- model/note-list-view.js
+++ model/note-list-view.js
@@ -4,14 +4,21 @@
   }
 
 NoteListView.prototype.viewNoteList = function () {
-  if (this.returnNoteList().all.length === 0) {
-    return '<ul><li><div>No notes saved</div></li></ul>'
-  } else {
-    return this.returnNoteList().all.map(function(note) {
-      return `<ul><li><div>${note}</div></li></ul>`
-    }).join(' ')
-  };
+  htmlString = '<ul>'
+  this.notelist.all.map(item => htmlString += '<li><div>' + item.substring(0,20) + '</div></li>' )
+  htmlString += '</ul>'
+  return htmlString
 };
+//
+// '
+  // if (this.returnNoteList().all.length === 0) {
+  //   return '<ul><li><div>No notes saved</div></li></ul>'
+  // } else {
+  //   return this.returnNoteList().all.map(function(note) {
+  //     return `<ul><li><div>${note}</div></li></ul>`
+  //   }).join(' ')
+  // };
+// };
 
 NoteListView.prototype.returnNoteList = function () {
   return this.notelist;
--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -1,38 +1,15 @@
+var notelist = new NoteList();
+var notelistview = new NoteListView(notelist);
+notelist.create('Favourite drink: seltzer')
+
 function testNoteListModelCreation() {
   console.log('takes a note list model upon instantiation:');
-  var notelist = new NoteList();
-  var notelistview = new NoteListView(notelist);
   assert.isTrue(notelistview.returnNoteList() === notelist);
 }
 testNoteListModelCreation();
 
-function testReturnsEmptyNoteListIfNoNotesSaved() {
-  console.log('returns "no notes saved" if notelist is empty:');
-  var notelist = new NoteList();
-  var notelistview = new NoteListView(notelist);
-  assert.isTrue(notelistview.viewNoteList() === '<ul><li><div>No notes saved</div></li></ul>');
-}
-testReturnsEmptyNoteListIfNoNotesSaved();
-
-// function testReturnsOneNote() {
-//   console.log('returns one note:');
-//   var note = new Note('First note')
-//   var notelist = new NoteList();
-//   notelist.create(note);
-//   var notelistview = new NoteListView(notelist);
-//   assert.isTrue(notelistview.viewNoteList() === '<ul><li><div>First note</div></li></ul>');
-// }
-// testReturnsOneNote();
-//
-// function testReturnsSeveralNotes() {
-//   console.log('returns several notes:');
-//   var note = new Note('First note')
-//   var note2 = new Note('Second note')
-//   var notelist = new NoteList();
-//   notelist.create(note);
-//   notelist.create(note2);
-//   var notelistview = new NoteListView(notelist);
-//   assert.isTrue(notelistview.viewNoteList() ===
-//     '<ul><li><div>First note</div></li></ul> <ul><li><div>Second note</div></li></ul>');
-// }
-// testReturnsSeveralNotes();
+function testViewFirstTwentyCharacters() {
+  console.log('Returns 20 first characters of each note')
+  assert.isTrue(notelistview.viewNoteList() === '<ul><li><div>Favourite drink: sel</div></li></ul>')
+};
+testViewFirstTwentyCharacters()
